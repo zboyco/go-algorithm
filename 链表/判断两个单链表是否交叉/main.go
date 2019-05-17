af0a02a32d92ad01d857e3033857caea05e7fb23
BaiYang <xliu779@foxmail.com>
2019-05-17 18:28:05 +0800

    提交

--- 链表/判断两个单链表是否交叉/main.go
+++ 链表/判断两个单链表是否交叉/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type node struct {
+	value int
+	next  *node
+}
+
+func main() {
+	s := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+	list := newLinkedList(s)
+
+	// cross(node)
+
+	printNode(list)
+}
+
+func cross(a, b *node) bool {
+	return false
+}
+
+func newLinkedList(s []int) *node {
+	head := &node{}
+	cur := head
+	for _, v := range s {
+		next := &node{
+			value: v,
+		}
+		cur.next = next
+		cur = next
+	}
+	return head
+}
+
+func printNode(head *node) {
+	next := head.next
+	for next != nil {
+		fmt.Print(fmt.Sprintf("%v -> ", next.value))
+		next = next.next
+	}
+	fmt.Println()
 }
